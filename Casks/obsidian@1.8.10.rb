4345766cd1d4116bce2369e4add6351d977e13f9
Miles <142267654+salver-shining-ramp@users.noreply.github.com>
2026-05-14 09:48:51 +0100

    Add Obsidian 1.8.10 pinned cask

---
+++ Casks/obsidian@1.8.10.rb
@@ -0,0 +1,11 @@
+cask "obsidian@1.8.10" do
+  version "1.8.10"
+  sha256 "dc188f6d3d4c13be56a51fe64c397cfd323ecaaebe58c53e45a657ca4506f321"
+
+  url "https://github.com/obsidianmd/obsidian-releases/releases/download/v1.8.10/Obsidian-1.8.10.dmg"
+  name "Obsidian"
+  desc "Knowledge base that works on top of a local folder of plain text Markdown files"
+  homepage "https://obsidian.md/"
+
+  app "Obsidian.app"
+end
